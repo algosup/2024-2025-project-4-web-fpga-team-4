fc961cb3476871ec41709f67583c103e630f24e6
Guillaume Despaux <146001117+GuillaumeDespaux@users.noreply.github.com>
2025-03-17 19:57:42 +0100

    Github Action clean + firefox versioning

--- src/test/firefox-unit-test-html-css.ts
+++ src/test/firefox-unit-test-html-css.ts
@@ -8,9 +8,11 @@ const date: Date = new Date();
 const dateString: string = date.toISOString().split(/T/)[0];
 const results: { 
   browser: string,
+  version: string | undefined,
   test: {
     name: string,
     description: string,
+    priority: string,
     type: string[],
     passed: boolean,
     error?: any
@@ -20,42 +22,42 @@ const results: {
 const logFilePath = path.join(__dirname, 'logs', dateString + '-firefox-unit-test-html-css.json');
 
 const firefoxOptions = new FirefoxOptions().addArguments('--headless');
-// const safariOptions = new SafariOptions();
 
+const versions = ['116.0', '117.0', '118.0', '119.0', '120.0', '121.0', '122.0', '123.0', '124.0', '125.0', '126.0', '127.0', '128.0', '129.0', '130.0', '131.0', '132.0', '133.0', '134.0', '135.0', '136.0'];
 
-async function runTests(browser: string, options: any) {
+async function runTests(browser: string, options: any, version: string) {
   let driver: WebDriver;
   driver = await new Builder().forBrowser(browser).setFirefoxOptions(options).build();
 
   try {
     await driver.get("https://two024-2025-project-4-web-fpga-team-4.onrender.com/client.html");
 
-    await runTest(driver, 'Title Test', 'Check if the title is correct', ["typo"], async () => {
+    await runTest(driver, version, 'Title Test', 'Check if the title is correct', 'high', ["typo"], async () => {
       const title = await driver.getTitle();
       assert.strictEqual("Client Side", title);
     });
 
     await driver.manage().setTimeouts({ implicit: 10000 });
 
-    await runTest(driver, 'Background Color Test', 'Check if the background color is correct', ["css", "html"], async () => {
+    await runTest(driver, version, 'Background Color Test', 'Check if the background color is correct', 'medium', ["css", "html"], async () => {
       const description = await driver.findElement(By.id('description')).getCssValue('background-color');
       assert.strictEqual("rgb(27, 38, 59)", description);
     });
     
-    await runTest(driver, 'Background Color Test', 'Check if the background color is correct in hex', ["css"],  async () => {
+    await runTest(driver, version, 'Background Color Test', 'Check if the background color is correct in hex', 'medium', ["css"],  async () => {
       const description = await driver.findElement(By.id('description')).getCssValue('background-color');
       const hexColor = rgbToHex(description);
       assert.strictEqual("#1B263B", hexColor);
     });
 
-    await runTest(driver, 'Speed Value Test', 'Check if the speed value is correct', ["html", "js"], async () => {
+    await runTest(driver, version, 'Speed Value Test', 'Check if the speed value is correct', 'low', ["html", "js"], async () => {
       const speed = await driver.findElement(By.id('speed')).getAttribute('value');
       const speedNumber = parseInt(speed);
       assert.strictEqual(1, speedNumber);
     });
 
   } catch (e) {
-    console.error(`Error running tests on ${browser}:`, e);
+    console.error(`Error running tests on ${browser} version ${version}:`, e);
   } 
   finally {
     if (driver) {
@@ -65,15 +67,16 @@ async function runTests(browser: string, options: any) {
   }
 }
 
-async function runTest(driver: WebDriver, testName: string, testDescription: string, testTypes: string[], testFn: () => Promise<void>) {
-  const browserName = (await driver.getCapabilities()).get('browserName');
+async function runTest(driver: WebDriver, browserName: string, testName: string, testDescription: string, testPriority: string, testTypes: string[], testFn: () => Promise<void>) {
   try {
     await testFn();
     results.push({ 
       browser: browserName,
+      version: (await driver.getCapabilities()).getBrowserVersion(),
       test: {
         name: testName,
         description: testDescription,
+        priority: testPriority,
         type: testTypes,
         passed: true
       }
@@ -81,9 +84,11 @@ async function runTest(driver: WebDriver, testName: string, testDescription: str
   } catch (e) {
     results.push({ 
       browser: browserName,
+      version: (await driver.getCapabilities()).getBrowserVersion(),
       test: {
         name: testName,
         description: testDescription,
+        priority: testPriority,
         type: testTypes,
         passed: false,
         error: e
@@ -91,6 +96,7 @@ async function runTest(driver: WebDriver, testName: string, testDescription: str
     });
   }
 }
+
 
 function logResults() {
   const logData = {
@@ -110,4 +116,8 @@ function rgbToHex(rgb: string): string {
   return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()}`;
 }
 
-runTests(Browser.FIREFOX, firefoxOptions);
+(async () => {
+  for (const version of versions) {
+    await runTests(Browser.FIREFOX, firefoxOptions, version);
+  }
+})();
