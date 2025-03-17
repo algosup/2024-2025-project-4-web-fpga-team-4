c8e6e397773a092da34d35eae99d48189090c104
Guillaume Despaux <146001117+GuillaumeDespaux@users.noreply.github.com>
2025-03-17 19:10:28 +0100

    Testing Edge environement

--- src/test/chrome-unit-test-html-css.ts
+++ src/test/chrome-unit-test-html-css.ts
@@ -22,26 +22,12 @@ const results: {
 
 const logFilePath = path.join(__dirname, 'logs', dateString + '-unit-test-html-css.json');
 
-const firefoxOptions = new FirefoxOptions().addArguments('--headless');
 const chromeOptions = new ChromeOptions().addArguments('--headless');
-const safariOptions = new SafariOptions();
 
 
 async function runTests(browser: string, options: any) {
   let driver: WebDriver;
-  switch (browser) {
-    case Browser.FIREFOX:
-      driver = await new Builder().forBrowser(browser).setFirefoxOptions(options).build();
-      break;
-    case Browser.CHROME:
-      driver = await new Builder().forBrowser(browser).setChromeOptions(options).build();
-      break;
-    case Browser.SAFARI:
-      driver = await new Builder().forBrowser(browser).setSafariOptions(options).build();
-      break;
-    default:
-      throw new Error('Invalid browser');
-  }
+    driver = await new Builder().forBrowser(browser).setChromeOptions(options).build();
 
   try {
     await driver.get("https://two024-2025-project-4-web-fpga-team-4.onrender.com/client.html");
@@ -126,8 +112,4 @@ function rgbToHex(rgb: string): string {
   return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()}`;
 }
 
-runTests(Browser.FIREFOX, firefoxOptions);
 runTests(Browser.CHROME, chromeOptions);
-
-runTests(Browser.SAFARI, safariOptions);
-
